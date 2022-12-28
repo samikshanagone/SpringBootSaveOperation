324794cd340c5db5444965ca85b05490419c18a9
samikshanagone <samikshanagone33@gmail.com>
2022-12-28 17:13:05 +0530

    Jump class is add

---
+++ src/main/java/com/example/demo/model/Jump.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+public class Jump {
+  
+  public static void main(String[] args) {
+	String str="samiksha is best";
+	
+	System.out.println(str);
+	
+}
+}
--- src/main/java/com/example/demo/model/jump.java
+++
@@ -1,4 +0,0 @@
-public class jump {
-  
-  //new class with code is added
-}
