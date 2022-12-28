8b45e41c1e106527656a3454cc4a3b78ab9f6a2e
samikshanagone <samikshanagone33@gmail.com>
2022-12-28 20:03:35 +0530

    merge

--- src/main/java/com/example/demo/controller/Demo.java
+++ src/main/java/com/example/demo/controller/Demo.java
@@ -1,10 +1,12 @@
+package com.example.demo.controller;
+
 public class Demo{
   
  public static void main(String[] args) {
 	String str="samiksha is best";
 	
 	System.out.println(str);
-	
+	System.out.println("hello samiksha ");
 }
   
 }
